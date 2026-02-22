fa71dd4bdfa47a3e7d35870f55a9826fe2d8dce1
Omid Shahbazi <sh.omid.m@gmail.com>
2026-02-21 18:20:47 -0800

    Modified   DaisyTTF.csproj Modified   Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using LunarLabs.Fonts;
 using System.Diagnostics;
+using System.Drawing;
 using System.Text;
+using Font = LunarLabs.Fonts.Font;
 
 namespace DaisyTTF
 {
@@ -8,7 +10,7 @@ namespace DaisyTTF
 	{
 		private struct Arguments
 		{
-			public string FontFilepath;
+			public string Path;
 			public uint Size;
 			public uint BitsPerPixel;
 			public bool AddGlyphData;
@@ -21,152 +23,284 @@ namespace DaisyTTF
 
 		private static void Main(string[] args)
 		{
-			args = new string[] { "Z:/calibrib.ttf", "32", "2" };
+			args = new string[] { "Z:/Pedal.png", "32", "4" };
+			//args = new string[] { "Z:/calibrib.ttf", "32", "2" };
 
 			Arguments arguments = GetArguments(args);
-			if (!File.Exists(arguments.FontFilepath))
-				throw new FileNotFoundException(arguments.FontFilepath);
+			if (!File.Exists(arguments.Path))
+				throw new FileNotFoundException(arguments.Path);
 
-			arguments.AddGlyphData = true;
 			//arguments.DebugView = true;
 
-			Font font = new Font(File.ReadAllBytes(arguments.FontFilepath));
+			string output = string.Empty;
 
-			string variableName = Path.GetFileNameWithoutExtension(arguments.FontFilepath);
-			variableName = variableName.Replace(" ", string.Empty);
-			variableName = variableName.Replace('-', '_');
-			variableName = $"Font_{variableName}_{arguments.Size}_{arguments.BitsPerPixel}BPP";
-
-			string output = BuildBitmapData(variableName, font, arguments);
+			if (Path.GetExtension(arguments.Path).ToLower() == ".ttf")
+				output = FontData.BuildData(arguments);
+			else
+				output = ImageData.BuildData(arguments);
 
 			Console.Write(output);
 		}
 
-		private static string BuildBitmapData(string variableName, Font font, Arguments arguments)
+		private static class ImageData
 		{
-			string dataVariableName = $"{variableName}_DATA";
-
-			StringBuilder output = new StringBuilder();
-			output.AppendLine($"static const uint{DATA_BIT_COUNT} {dataVariableName}[] = {{");
-
-			//uint maxWidth = 0;
-			//uint width;
-			//AppendGlyphData(output, font, 'C', arguments, out width);
-
-			uint maxWidth = 0;
-			for (char c = FIRST_CHARACTER; c <= LAST_CHARACTER; ++c)
+			public static string BuildData(Arguments arguments)
 			{
-				uint width;
-				AppendGlyphData(output, font, c, arguments, out width);
+				Bitmap bitmap = GetBitmap(arguments);
 
-				if (width > maxWidth)
-					maxWidth = width;
+				return BuildData(bitmap, arguments);
 			}
 
+			private static string BuildData(Bitmap bitmap, Arguments arguments)
+			{
+				string variableName = GetVariableName(arguments, "Bitmap");
+				string dataVariableName = $"{variableName}_DATA";
+
+				StringBuilder output = new StringBuilder();
+
+				BuildHeader(output, dataVariableName);
+
+				uint pixelCountPerElement = Math.Min(arguments.Size, DATA_BIT_COUNT / arguments.BitsPerPixel);
+				uint elementCountPerRow = Math.Max(1, arguments.Size / (DATA_BIT_COUNT / arguments.BitsPerPixel));
+				uint bitsPerChannel = Math.Min(2, arguments.BitsPerPixel);
+
+				uint totalWrittenElementCount = 0;
+				for (uint y = 0; y < bitmap.Height; ++y)
+				{
+					uint writtenBitCount = 0;
+					uint writtenElementCount = 0;
+					ulong data = 0;
+					void flush()
+					{
+						if (arguments.DebugView)
+							output.Append($"0b{data.ToString($"B64")}, ");
+						else
+							output.Append($"0x{data.ToString($"X16")}, ");
+
+						writtenBitCount = 0;
+						data = 0;
+
+						++writtenElementCount;
+						++totalWrittenElementCount;
+					}
+
+					for (uint x = 0; x < bitmap.Width; ++x)
+					{
+						Color pixel = bitmap.GetPixel((int)x, (int)y);
+						uint bitOffset = x * arguments.BitsPerPixel;
+
+						//Write Alpha Multiplier
+						{
+							byte b = PixelValueToBit(pixel.A, bitsPerChannel);
+							data |= ((ulong)b << (int)bitOffset);
+						}
+
+						//Write Color Multiplier
+						if (arguments.BitsPerPixel == 4)
+						{
+							byte b = PixelValueToBit(pixel.R, bitsPerChannel);
+							data |= ((ulong)b << (int)(bitOffset + bitsPerChannel));
+						}
+
+						writtenBitCount += arguments.BitsPerPixel;
+
+						if ((x + 1) % pixelCountPerElement == 0)
+							flush();
+					}
+
+					for (; writtenElementCount < elementCountPerRow; ++writtenElementCount)
+						flush();
+
+					if (arguments.DebugView)
+						output.AppendLine();
+				}
+
+				output.AppendLine();
+
+				BuildFooter(output);
+
+				output.AppendLine($"static const Bitmap {variableName} = {{{bitmap.Width}, {bitmap.Height}, {dataVariableName}, {arguments.BitsPerPixel}}};");
+
+				return output.ToString();
+			}
+
+			private static Bitmap GetBitmap(Arguments arguments)
+			{
+				Image image = Image.FromFile(arguments.Path);
+				if (image.Height != arguments.Size)
+				{
+					float ratio = (float)arguments.Size / image.Height;
+
+					image = new Bitmap(image, (int)(image.Width * ratio), (int)(image.Height * ratio));
+				}
+
+				return new Bitmap(image);
+			}
+		}
+
+		private static class FontData
+		{
+			public static string BuildData(Arguments arguments)
+			{
+				arguments.AddGlyphData = true;
+
+				Font font = new Font(File.ReadAllBytes(arguments.Path));
+
+				return BuildData(font, arguments);
+			}
+
+			private static string BuildData(Font font, Arguments arguments)
+			{
+				string variableName = GetVariableName(arguments, "Font");
+				string dataVariableName = $"{variableName}_DATA";
+
+				StringBuilder output = new StringBuilder();
+
+				BuildHeader(output, dataVariableName);
+
+				//uint maxWidth = 0;
+				//uint width;
+				//AppendGlyphData(output, font, 'C', arguments, out width);
+
+				uint maxWidth = 0;
+				for (char c = FIRST_CHARACTER; c <= LAST_CHARACTER; ++c)
+				{
+					uint width;
+					AppendGlyphData(output, font, c, arguments, out width);
+
+					if (width > maxWidth)
+						maxWidth = width;
+				}
+
+				BuildFooter(output);
+
+				output.AppendLine($"static const Font {variableName} = {{{maxWidth}, {arguments.Size}, {dataVariableName}, 1, {arguments.BitsPerPixel}, '{FIRST_CHARACTER}', '{LAST_CHARACTER}', {arguments.AddGlyphData.ToString().ToLower()}}};");
+
+				return output.ToString();
+			}
+
+			private static void AppendGlyphData(StringBuilder output, Font font, char c, Arguments arguments, out uint width)
+			{
+				FontGlyph glyph = font.RenderGlyph(c, font.ScaleInPixels(arguments.Size));
+				GlyphBitmap image = glyph.Image;
+
+				width = (uint)glyph.xAdvance;
+
+				output.Append("\t");
+
+				int xOfs = glyph.xOfs;
+				int yOfs = (int)(glyph.yOfs + arguments.Size);
+
+				if (arguments.AddGlyphData)
+				{
+					ulong glyphData = 0;
+
+					int bitOffset = 0;
+
+					glyphData |= (width & 0xFF) << bitOffset;
+					bitOffset += sizeof(byte) * 8;
+
+					glyphData |= ((ulong)xOfs & 0xFF) << bitOffset;
+					bitOffset += sizeof(byte) * 8;
+
+					glyphData |= ((ulong)yOfs & 0xFF) << bitOffset;
+					bitOffset += sizeof(byte) * 8;
+
+					Debug.Assert(bitOffset < DATA_BIT_COUNT);
+
+					if (arguments.DebugView)
+						output.AppendLine($"0b{glyphData.ToString($"B64")}, ");
+					else
+						output.Append($"0x{glyphData.ToString($"X16")}, ");
+				}
+
+				uint pixelCountPerElement = Math.Min(arguments.Size, DATA_BIT_COUNT / arguments.BitsPerPixel);
+				uint elementCountPerRow = Math.Max(1, arguments.Size / (DATA_BIT_COUNT / arguments.BitsPerPixel));
+
+				uint totalWrittenElementCount = 0;
+
+				for (uint y = 0; y < arguments.Size; ++y)
+				{
+					int indexY = (int)y - (glyph.yOfs + (int)arguments.Size);
+
+					uint writtenBitCount = 0;
+					uint writtenElementCount = 0;
+					ulong data = 0;
+					void flush()
+					{
+						if (arguments.DebugView)
+							output.Append($"0b{data.ToString($"B64")}, ");
+						else
+							output.Append($"0x{data.ToString($"X16")}, ");
+
+						writtenBitCount = 0;
+						data = 0;
+
+						++writtenElementCount;
+						++totalWrittenElementCount;
+					}
+
+					for (uint x = 0; x < arguments.Size; ++x)
+					{
+						int indexX = (int)x - glyph.xOfs;
+
+						if (x < image.Width && y < image.Height)
+						{
+							byte pixelValue = image.Pixels[x + (y * image.Width)];
+
+							byte b = PixelValueToBit(pixelValue, arguments.BitsPerPixel);
+
+							data |= ((ulong)b << (int)(x * arguments.BitsPerPixel));
+						}
+
+						writtenBitCount += arguments.BitsPerPixel;
+
+						if ((x + 1) % pixelCountPerElement == 0)
+							flush();
+					}
+
+					for (; writtenElementCount < elementCountPerRow; ++writtenElementCount)
+						flush();
+
+					if (arguments.DebugView)
+						output.AppendLine();
+				}
+
+				output.Append($"// [{c}]");
+
+				if (arguments.AddGlyphData)
+					output.Append($" Width [{width}] Offset [{xOfs}, {yOfs}]");
+
+				output.AppendLine();
+
+				Debug.Assert(totalWrittenElementCount == elementCountPerRow * arguments.Size);
+			}
+		}
+
+		private static void BuildHeader(StringBuilder output, string dataVariableName)
+		{
+			output.AppendLine($"// Header");
+			output.AppendLine($"extern const uint{DATA_BIT_COUNT} {dataVariableName}[];");
+
+			output.AppendLine($"// CPP");
+			output.AppendLine($"const uint{DATA_BIT_COUNT} {dataVariableName}[] = {{");
+		}
+
+		private static void BuildFooter(StringBuilder output)
+		{
 			output.AppendLine("};");
 
 			output.AppendLine("");
-
-			output.AppendLine($"static const Font {variableName} = {{{maxWidth}, {arguments.Size}, {dataVariableName}, 1, {arguments.BitsPerPixel}, '{FIRST_CHARACTER}', '{LAST_CHARACTER}', {arguments.AddGlyphData.ToString().ToLower()}}};");
-
-			return output.ToString();
 		}
 
-		private static void AppendGlyphData(StringBuilder output, Font font, char c, Arguments arguments, out uint width)
+		private static string GetVariableName(Arguments arguments, string Type)
 		{
-			FontGlyph glyph = font.RenderGlyph(c, font.ScaleInPixels(arguments.Size));
-			GlyphBitmap image = glyph.Image;
+			string variableName = Path.GetFileNameWithoutExtension(arguments.Path);
+			variableName = variableName.Replace(" ", string.Empty);
+			variableName = variableName.Replace('-', '_');
+			variableName = $"{Type}_{variableName}_{arguments.Size}_{arguments.BitsPerPixel}BPP";
 
-			width = (uint)glyph.xAdvance;
-
-			output.Append("\t");
-
-			int xOfs = glyph.xOfs;
-			int yOfs = (int)(glyph.yOfs + arguments.Size);
-
-			if (arguments.AddGlyphData)
-			{
-				ulong glyphData = 0;
-
-				int bitOffset = 0;
-
-				glyphData |= (width & 0xFF) << bitOffset;
-				bitOffset += sizeof(byte) * 8;
-
-				glyphData |= ((ulong)xOfs & 0xFF) << bitOffset;
-				bitOffset += sizeof(byte) * 8;
-
-				glyphData |= ((ulong)yOfs & 0xFF) << bitOffset;
-				bitOffset += sizeof(byte) * 8;
-
-				Debug.Assert(bitOffset < DATA_BIT_COUNT);
-
-				if (arguments.DebugView)
-					output.AppendLine($"0b{glyphData.ToString($"B64")}, ");
-				else
-					output.Append($"0x{glyphData.ToString($"X16")}, ");
-			}
-
-			uint pixelCountPerElement = Math.Min(arguments.Size, DATA_BIT_COUNT / arguments.BitsPerPixel);
-			uint elementCountPerRow = Math.Max(1, arguments.Size / (DATA_BIT_COUNT / arguments.BitsPerPixel));
-
-			uint totalWrittenElementCount = 0;
-
-			for (uint y = 0; y < arguments.Size; ++y)
-			{
-				int indexY = (int)y - (glyph.yOfs + (int)arguments.Size);
-
-				uint writtenBitCount = 0;
-				uint writtenElementCount = 0;
-				ulong data = 0;
-				void flush()
-				{
-					if (arguments.DebugView)
-						output.Append($"0b{data.ToString($"B64")}, ");
-					else
-						output.Append($"0x{data.ToString($"X16")}, ");
-
-					writtenBitCount = 0;
-					data = 0;
-
-					++writtenElementCount;
-					++totalWrittenElementCount;
-				}
-
-				for (uint x = 0; x < arguments.Size; ++x)
-				{
-					int indexX = (int)x - glyph.xOfs;
-
-					if (x < image.Width && y < image.Height)
-					{
-						byte pixelValue = image.Pixels[x + (y * image.Width)];
-
-						byte b = PixelValueToBit(pixelValue, (int)arguments.BitsPerPixel);
-
-						data |= ((ulong)b << (int)(x * arguments.BitsPerPixel));
-					}
-
-					writtenBitCount += arguments.BitsPerPixel;
-
-					if ((x + 1) % pixelCountPerElement == 0)
-						flush();
-				}
-
-				for (; writtenElementCount < elementCountPerRow; ++writtenElementCount)
-					flush();
-
-				if (arguments.DebugView)
-					output.AppendLine();
-			}
-
-			output.Append($"// [{c}]");
-
-			if (arguments.AddGlyphData)
-				output.Append($" Width [{width}] Offset [{xOfs}, {yOfs}]");
-
-			output.AppendLine();
-
-			Debug.Assert(totalWrittenElementCount == elementCountPerRow * arguments.Size);
+			return variableName;
 		}
 
 		private static Arguments GetArguments(string[] args)
@@ -176,10 +310,10 @@ namespace DaisyTTF
 
 			Arguments arguments = new Arguments();
 
-			arguments.FontFilepath = args[0];
+			arguments.Path = args[0];
 
 			arguments.Size = Convert.ToUInt32(args[1]);
-			arguments.Size = Math.Min(64, Math.Max(8, arguments.Size));
+			arguments.Size = Math.Min(DATA_BIT_COUNT, Math.Max(8, arguments.Size));
 
 			arguments.BitsPerPixel = Convert.ToUInt32(args[2]);
 			arguments.BitsPerPixel = Math.Min(4, Math.Max(1, arguments.BitsPerPixel));
@@ -187,7 +321,7 @@ namespace DaisyTTF
 			return arguments;
 		}
 
-		private static byte PixelValueToBit(byte pixelValue, int bitsPerPixel)
+		private static byte PixelValueToBit(byte pixelValue, uint bitsPerPixel)
 		{
 			if (bitsPerPixel == 1)
 			{
